f47374f6f7ef01ec2b3c7b18b73018b8abaa241f
lee misterk <leemisterk.dev@gmail.com>
2023-10-31 16:21:43 +0700

    create Car model

--- mypj/car/models.py
+++ mypj/car/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Car(models.Model):
+    name=models.CharField(max_length=50)
+    image=models.ImageField(upload_to='images')
+    sound=models.FileField(upload_to='sounds')
+
+    def __str__(self):
+        return self.name
