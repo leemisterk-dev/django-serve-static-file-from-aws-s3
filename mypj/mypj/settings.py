97f8283b96d251a928092da8f3b0fcc389146565
lee misterk <leemisterk.dev@gmail.com>
2023-10-31 16:18:19 +0700

    install pillow django-storages

--- mypj/mypj/settings.py
+++ mypj/mypj/settings.py
@@ -11,7 +11,10 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-from 
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -22,7 +25,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-sp%ljjnefischk-kst-$g0@9l48bs93n%znxq(9)m@v=_tp-ot'
+# SECRET_KEY = 'django-insecure-sp%ljjnefischk-kst-$g0@9l48bs93n%znxq(9)m@v=_tp-ot'
+SECRET_KEY= os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
